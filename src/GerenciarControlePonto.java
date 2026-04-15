4d1cd0f3f34580a3108b2fc4ad3853ec96d86bfc
FelipeSoft <felipedecastrolima2@gmail.com>
2026-04-14 21:29:17 -0300

    feat: including others setters

--- src/GerenciarControlePonto.java
+++ src/GerenciarControlePonto.java
@@ -7,12 +7,26 @@ public class GerenciarControlePonto {
     public static void main(String[] args) throws InterruptedException {
         Gerente gerente = new Gerente();
         gerente.setNome("Gerente");
+        gerente.setDocumento("12345678910");
+        gerente.setEmail("gerente@domain.com");
+        gerente.setIdFunc(1);
+        gerente.setLogin("gerente");
+        gerente.setSenha("gerente");
 
         Secretaria secretaria = new Secretaria();
         secretaria.setNome("Secretaria");
+        secretaria.setDocumento("12345678911");
+        secretaria.setEmail("secretaria@domain.com");
+        secretaria.setIdFunc(2);
+        secretaria.setTelefone("1999988776666");
+        secretaria.setRamal("256");
 
         Operador operador = new Operador();
         operador.setNome("Operador");
+        operador.setDocumento("12345678912");
+        operador.setEmail("operador@domain.com");
+        operador.setIdFunc(3);
+        operador.setValorHora(20.5);
 
         RegistroPonto registroPontoGerente = new RegistroPonto();
         registroPontoGerente.setFunc(gerente);
